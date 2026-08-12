b6425e40909f5c1a98921fa87fe1d5be4d87355d
Vinicius Vieira Romão <vieiraromaovinicius@gmail.com>
2026-08-12 19:46:06 -0300

    Trabalhando com o block e endblock, utilizei um laço FOR dentro da própria página HTML através de variáveis de contexto extraídas do banco de dados através das views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from . models import Topic
+
 
 def index(request):
     """Página principal do Learning_Log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
